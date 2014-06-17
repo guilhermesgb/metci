bde7c63a2a514689626475578ada05c8d0fd5ca4
Guilherme Baptista <guisgb13@gmail.com>
2014-06-17 05:27:50 +0200

    Very simple HashMap impl also

--- project.py
+++ project.py
@@ -1,17 +1,32 @@
-class ListsDict:
+class ListDict:
 
     def __init__(self):
         self.storage = []
 
     def insert(word):
+        word = word.lower()
         try:
             self.storage.index(word)
         except ValueError:
             self.storage.append(word)
 
     def lookup(word):
+        word = word.lower()
         try:
             self.storage.index(word)
             return True
         except ValueError:
             return False
+
+class HashMapDict:
+
+    def __init__(self):
+        self.storage = {}
+
+    def insert(word):
+        word = word.lower()
+        self.storage[word] = True
+
+    def lookup(word):
+        word = word.lower()
+        return True if self.storage.get(word, False) else False

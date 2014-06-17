714ac3d6bb5d6f81b94d85e0b952771f53806999
Guilherme Baptista <guisgb13@gmail.com>
2014-06-17 05:56:38 +0200

    Very simple Binary Search Tree Dict impl

--- project.py
+++ project.py
@@ -30,3 +30,39 @@ class HashMapDict:
     def lookup(word):
         word = word.lower()
         return True if self.storage.get(word, False) else False
+
+class BinarySearchTreeDict:
+
+    class Node:
+        def __init__(self, left, word, right):
+            self.left = left
+            self.word = word
+            self.right = right
+
+    def __init__(self):
+        self.storage = None
+
+    def insert(word):
+        _insert(word.lower())
+
+    def _insert(word, node=self.storage):
+        word = word.lower()
+        if ( node == None ):
+            node = Node(None, word, None)
+        elif ( word < node.word ):
+            self.insert(word, node=node.left)
+        elif ( word > node.word ):
+            self.insert(word, node=node.right)
+
+    def lookup(word):
+        return _lookup(word.lower())
+
+    def _lookup(word, node=self.storage):
+        if ( node == None ):
+            return False
+        elif ( word == node.word ):
+            return True
+        elif ( word < node.word ):
+            return self.lookup(word, node=node.left)
+        else:
+            return self.lookup(word, node=node.right)
